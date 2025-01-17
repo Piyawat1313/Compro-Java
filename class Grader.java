8213eb2945687bf877354da52636a403edaefc3f
Piyawat1313 <Piyawatbiwkai@gmail.com>
2025-01-17 12:08:33 +0700

    Create class Grader.java

---
+++ class Grader.java
@@ -0,0 +1,12 @@
+class Grader{
+    String name;
+    String desc;
+    int[] pts;
+    String[] sols;
+    public Grader(String name,String desc,int[] pts, String[] sols){
+        this.name = name;
+        this.desc = desc;
+        this.pts = java.util.Arrays.copyOf(pts,pts.length);
+        this.sols = java.util.Arrays.copyOf(sols, sols.length);
+    }
+}
