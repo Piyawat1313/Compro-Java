c960c584fdbc5e40da4c72c3203447d13560e6c1
Piyawat1313 <Piyawatbiwkai@gmail.com>
2025-04-21 15:03:02 +0700

    Update Vote.java

--- Vote.java
+++ Vote.java
@@ -2,28 +2,24 @@ import java.util.Scanner;
 public class Vote {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input N: ");
         int n = sc.nextInt();
-            System.out.print("Input K: ");   
         int k = sc.nextInt();
-            int[] vote = new int[n];
-            int[] sum_vote = new int[k + 1];
-            System.out.print("Input Vote: ");
-            for(int i = 0; i < n; i++){
-                    vote[i] = sc.nextInt();
-                    if(vote[i] >= 1 && vote[i] <= k){
-                        sum_vote[vote[i]]++;
-                    }   
+        int arr[] = new int[101];
+        for(int i = 0; i < k; i++){
+            int vote = sc.nextInt();
+            arr[vote]++;     //บันทึกว่าช่องนี้มีคนเลือกและนับคะแนน
+            //System.out.println(vote + " " + arr[vote]);
+        }
+        int max = 0;
+        int canidate = 0;
+        for(int i = 1; i <= n; i++){
+            if(arr[i] > max){
+                max = arr[i];
+                canidate = i;   //ช่องที่มีคนเลือกมากที่สุด
             }
-            int max_vote = 0;
-            int max_canidate = 0;
-            for(int i = 1; i <= k; i++){
-                if(sum_vote[i] > max_vote){
-                    max_vote = sum_vote[i];
-                    max_canidate = i;
-                }
-            }
-            System.out.println("Max_vote: "+ max_vote);
-            System.out.println("Canidate : " + max_canidate);
+        }
+        System.out.println(canidate);
+        System.out.println(max);
+        sc.close();
     }
 }
