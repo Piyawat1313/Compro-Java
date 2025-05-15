8b102dc2bef75efffe3120599d71a23b9b72d8ff
Piyawat1313 <Piyawatbiwkai@gmail.com>
2025-05-15 15:05:20 +0700

    Update RemoveMe.java

--- RemoveMe.java
+++ RemoveMe.java
@@ -2,41 +2,29 @@ import java.util.Scanner;
 
 public class RemoveMe {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-        int n = scanner.nextInt();
-        scanner.nextLine(); // Consume the newline
-
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int n = sc.nextInt();
+        int index = 0;
         for (int i = 0; i < n; i++) {
-            String[] command = scanner.nextLine().split(" ");
-            int direction = Integer.parseInt(command[0]);
-            String word = command[1];
-
-            if (direction == 1) {
-                text = removeFirstOccurrence(text, word);
-            } else if (direction == 2) {
-                text = removeLastOccurrence(text, word);
+            int number = sc.nextInt();
+            String s2 = sc.next();
+            sc.nextLine();
+            if (number == 1) {
+                index = s.indexOf(s2);
             }
-
-            System.out.println(text);
+            else{
+                index = s.lastIndexOf(s2);
+            }
+            if(index != -1){
+               String s4 = s.substring(0, index);
+               String s5 = s.substring(index + s2.length());
+                s = s4 + s5;
+            }
+            System.out.println(s);
         }
 
-        scanner.close();
-    }
-
-    private static String removeFirstOccurrence(String text, String word) {
-        int index = text.indexOf(word);
-        if (index != -1) {
-            return text.substring(0, index) + text.substring(index + word.length());
-        }
-        return text;
-    }
-
-    private static String removeLastOccurrence(String text, String word) {
-        int index = text.lastIndexOf(word);
-        if (index != -1) {
-            return text.substring(0, index) + text.substring(index + word.length());
-        }
-        return text;
+        sc.close();
     }
 }
+
