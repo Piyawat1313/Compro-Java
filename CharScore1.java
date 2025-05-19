9ed7cc549c4e4ebdd02df6cc5d24b8ee2cfdf893
Piyawat1313 <Piyawatbiwkai@gmail.com>
2025-05-19 19:03:47 +0700

    Update CharScore1.java

--- CharScore1.java
+++ CharScore1.java
@@ -1,23 +1,28 @@
 import java.util.Scanner;
+
 public class CharScore1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        int halfFirst = 0;
-        int halfBack = 0;
-        String text1 = "", text2 = "";
-            int halfCut = str.length() / 2;
-            text1 = str.substring( 0 ,halfCut);
-            text2 = str.substring(halfCut);
-         
-            for(char c : text1.toCharArray()){      //วิธีกำหนดค่า ให้ A = 1 .... ไปเรื่อยๆ
-            halfFirst += (c - 'A' + 1);
-         } 
-         for(char C : text2.toCharArray()){
-            halfBack += (C - 'A' + 1);
-         }  
-        System.out.println(halfFirst);
-        System.out.println(halfBack);
+        String word = sc.nextLine();
+        int median = word.length() / 2;
+        String sub = word.substring(0, median);
+        String sub1 = word.substring(median);
+        int sum = 0;
+        int score = 0;
+        for (int i = 0; i < sub.length(); i++) {        //ครึ่งแรก
+            char ch = sub.charAt(i);
+            score = ch - 'A' + 1;    //วิธีการกำหนดค่าให้ A = 1 ไล่ลงไปเรื่อยๆ
+            sum += score;
+        }
+        System.out.println(sum);
+        int score1 = 0;
+        int sum1 = 0;
+        for (int i = 0; i < sub1.length(); i++) {        //ครึ่งหลัง
+            char ch = sub1.charAt(i);
+            score1 = ch - 'A' + 1;
+            sum1 += score1;
+        }
+        System.out.println(Math.abs(sum1));
         sc.close();
     }
 }
