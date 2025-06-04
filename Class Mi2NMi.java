34e053ef4936604387f9056f1d44cacaa7ac7a73
Piyawat1313 <Piyawatbiwkai@gmail.com>
2025-06-04 16:10:29 +0700

    Update Class Mi2NMi.java

--- Class Mi2NMi.java
+++ Class Mi2NMi.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 
 public class Mi2NMi {
-    double nmi;
-    double mtonmi(double mi){
-        return this.nmi = mi * 0.868976;
+    static double mi2nmi(double m){
+        double miSea = m * 0.868976;
+        return miSea;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double mi = sc.nextDouble();
-        Mi2NMi M = new Mi2NMi();
-        System.out.println(M.mtonmi(mi) + " " + "nmi");  
-        sc.close();  
+        double m = sc.nextDouble();
+        System.out.println(mi2nmi(m) + " nmi");
+        sc.close();
     }
 }
