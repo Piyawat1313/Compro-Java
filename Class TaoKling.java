f8a5486117fec3a3913e59af59b16455799cba87
Piyawat1313 <Piyawatbiwkai@gmail.com>
2025-06-04 19:38:56 +0700

    Update Class TaoKling.java

--- Class TaoKling.java
+++ Class TaoKling.java
@@ -1,65 +1,68 @@
 import java.util.Scanner;
-
-class TaoKlingOrder {
+class TaoKlingOrder{
     int numMilk;
     int numJuice;
     int numMineralWater;
-    int total = 0;
-    String phoneNo = "";
+    int total;
+    String PhoneNo = "";
     String code;
     boolean completed;
-
-    public TaoKlingOrder(String phoneNo, int n1, int n2, int n3,  String code){
-        this.numMilk = n1;
-        this.numJuice = n2;
-        this.numMineralWater = n3;
-        this.code = code;
-        this.completed = false;
-        try {
-            if(phoneNo.startsWith("0") && phoneNo.length() == 10){
-                this.phoneNo = phoneNo;
+    TaoKlingOrder(String PhoneNo,int numMilk, int numJuice, int numMineralWater, String code){
+        if(numMilk > 0){
+            this.numMilk = numMilk;
+        }
+        else{
+            numMilk = 0;
+        }
+        if(numJuice > 0){
+            this.numJuice = numJuice;
+        }
+        else{
+            numJuice = 0;
+        }
+        if(numMineralWater > 0){
+            this.numMineralWater = numMineralWater;
+        }
+        else{
+            numMineralWater = 0;
+        }
+            if(PhoneNo.startsWith("0") && PhoneNo.length() == 10){
+                this.PhoneNo = PhoneNo;
              }else{
                  System.out.println("invalid phone number");
-                 this.phoneNo += "0#########";
+                 this.PhoneNo += "0#########";
              }
-        } catch (Exception e) {
-
-        }
-        
-        if(numMilk >= 0 && numJuice >= 0 && numMineralWater >= 0){
-            this.total += (numMilk * 10) +  (numJuice * 20) + (numMineralWater * 15);
-        }
+        this.code = code;
+        this.total = (this.numMilk * 10) + (this.numJuice * 20) + (this.numMineralWater * 15);
     }
-    boolean grabOrder(String phoneNo, String code){
-       if(this.phoneNo.equals(phoneNo) && this.code.equals(code)){
+    boolean grabOrder(String PhoneNo, String code){
+       if(this.PhoneNo.equals(PhoneNo) && this.code.equals(code)){
          this.completed = true;
         return true;
        }
        return false;
     }
     void printInfo(){
-        System.out.print(phoneNo + " ");
-        System.out.print(this.numMilk + " ");
-        System.out.print(this.numJuice + " ");
-        System.out.print(this.numMineralWater + " ");
-        System.out.print(this.total + " ");
-        if(completed) System.out.print("Y");
-        if(!completed) System.out.print("N");
+        if(completed){
+             System.out.print(this.PhoneNo + " " + numMilk + " " + numJuice + " " + numMineralWater + " " + this.total + " " + "Y");
+        } 
+        if(!completed){
+             System.out.print(this.PhoneNo + " " + numMilk + " " + numJuice + " " + numMineralWater + " " + this.total + " " + "N");
+        }
     }
 }
-
 public class TaoKling {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String phoneNo = sc.next();
-        int n1 = sc.nextInt();  //นม
-        int n2 = sc.nextInt();  //นำ้ผลไม้
-        int n3 = sc.nextInt();  //น้ำแร่
-        String code = sc.next();    //code คนแรก
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        String code = sc.next();
         TaoKlingOrder t = new TaoKlingOrder(phoneNo, n1, n2, n3, code);
-        String phone = sc.next();   //เบอร์โทรรอบสอง
-        String code1 = sc.next();   //code รอบที่ 2 
-        boolean flag = t.grabOrder(phone, code1);   
+        String phone = sc.next();
+        String code1 = sc.next();
+        boolean flag = t.grabOrder(phone, code1);
         System.out.println(flag);
         t.printInfo();
         sc.close();
