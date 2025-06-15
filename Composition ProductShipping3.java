8cdb9fd88366b21a52488a37fc1b442800c7474c
Biw_CS. <Piyawatbiwkai@gmail.com>
2025-06-15 22:20:22 +0700

    Create Composition ProductShipping3.java

---
+++ Composition ProductShipping3.java
@@ -0,0 +1,116 @@
+// package Compositionjava;
+
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+
+class Product {
+    String name;
+    int weight;
+    int price;
+
+    Product(String name, int weight, int price) {
+        this.name = name;
+        this.weight = weight;
+        this.price = price;
+    }
+
+    void printInfo() {
+        System.out.println(this.weight + " grams of " + this.name + " costs " + this.price + " baht");
+    }
+}
+
+class PurchaseOrder {
+    int countPro;
+    Product pro[];
+    int count[];
+    int price;
+    double weight;
+
+    PurchaseOrder(int countPro, Product pro[], int count[]) {
+        this.countPro = countPro;
+        this.count = new int[countPro];
+        this.pro = new Product[countPro];
+        this.pro = pro.clone();
+        this.count = count.clone();
+        for (int i = 0; i < count.length; i++) {
+            price += count[i] * pro[i].price;
+            weight += pro[i].weight * count[i];
+        }
+    }
+
+    void printInfo() {
+        System.out.println(this.countPro + " item = " + this.price + " bath " + this.weight + " grams ");
+        for (int i = 0; i < count.length; i++) {
+            System.out.println(pro[i].name + " " + this.count[i] + " x " + pro[i].price + " = "
+                    + String.format("%d", count[i] * pro[i].price));
+        }
+    }
+}
+
+class Truck {
+    int capacity;
+    PurchaseOrder preorder;
+    double stationfinal;
+    double shipping;
+
+    Truck(int capacity) {
+        this.capacity = capacity;
+        this.stationfinal = 0;
+        this.shipping = 0;
+    }
+
+    void add(PurchaseOrder preorder, int station) {
+        this.stationfinal = station;
+        this.preorder = preorder;
+        this.shipping = setFee();
+    }
+
+    double setFee() {
+        double fee = (preorder.weight / 1000.0) * this.stationfinal;
+        return fee;
+    }
+
+    String getFee() {
+         return String.format("%.2f", this.shipping);
+    }
+}
+
+public class ProductShipping3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        Truck[] trucks = new Truck[m];
+        for (int i = 0; i < m; i++) {
+            int capacity = in.nextInt();
+            trucks[i] = new Truck(capacity);
+        }
+        int n = in.nextInt();
+        int dest = in.nextInt();
+        Product[] products = new Product[n];
+        int[] amount = new int[n];
+        for (int i = 0; i < n; i++) {
+            String name = in.next();
+            int weight = in.nextInt();
+            int price = in.nextInt();
+            products[i] = new Product(name, weight, price);
+            amount[i] = in.nextInt();
+        }
+        PurchaseOrder po = new PurchaseOrder(n, products, amount);
+        int minvalue = Integer.MAX_VALUE;
+        int minIndex = -1;
+        for (int i = 0; i < m; i++) {
+            if ((trucks[i].capacity >= po.weight) && (minvalue > trucks[i].capacity)) {
+                minvalue = trucks[i].capacity;
+                minIndex = i;
+            }
+        }
+        if (minIndex >= 0) {
+            trucks[minIndex].add(po, dest);
+            System.out.println("Truck#" + (minIndex + 1));
+            System.out.println("Fee=" + trucks[minIndex].getFee());
+        } else {
+            System.out.println("No truck");
+            System.out.println("Fee=" + po.weight * dest / 1000.0);
+        }
+    }
+}
